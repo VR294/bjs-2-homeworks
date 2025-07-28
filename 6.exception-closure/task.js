ca2357f51edf82972c2351aff9491dda0aedf2bb
Viktoria Rasikhina <vicktoriarasihina@yandex.ru>
2025-07-29 02:25:26 +0300

    Шестое задание готово

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,45 @@
-﻿
+﻿function parseCount(value) {
+    const parsed = Number.parseFloat(value);
+    if (isNaN(parsed)) {
+        throw new Error("Невалидное значение");
+    };
+    return parsed;
+}
+
+function validateCount(val) {
+    try {
+        return parseCount(val);
+    } catch(error) {
+        return error;
+    }
+}
+
+class Triangle {
+    constructor(a, b, c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+        if (a + b <= c || a + c <= b || c + b <= a) {
+            throw new Error("Треугольник с такими сторонами не существует");
+        }
+    }
+    get perimeter() {
+        return this.a + this.b + this.c;
+    }
+    get area() {
+        const p = this.perimeter / 2;
+        const area = Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c));
+        return parseFloat(area.toFixed(3));
+    }
+}
+
+function getTriangle(a, b, c) {
+    try {
+        return new Triangle(a, b, c);
+    } catch(error) {
+        return {
+            get area() {return "Ошибка! Треугольник не существует"},
+            get perimeter() {return "Ошибка! Треугольник не существует"}
+        };
+    }
+}
